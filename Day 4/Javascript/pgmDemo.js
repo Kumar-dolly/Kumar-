0ac674870746e40e64cb054a4cd752512b736f37
Kumar-dolly <kumar17112006@gmail.com>
2025-10-14 14:46:06 +0530

    Add files via upload

---
+++ Day 4/Javascript/pgmDemo.js
@@ -0,0 +1,22 @@
+ var msg = "I am Kumar";
+        console.log(msg);
+
+        let mssg = "I am Kumar";
+        console.log(mssg);
+
+        const greeting = {
+        text:"welcome!",
+            sender: "System"
+             };
+             console.log(greeting);
+
+                function helloFunction() {
+                    console.log("hello from function");
+                }
+
+                helloFunction();
+
+                helloArrowFunction = () => {
+                    console.log('Hello From Arrow Function');
+                }
+                helloArrowFunction();
