9e597b4ca1056080c11485a836ba345251ebb0e5
Kumar.S <kumar17112006@gmail.com>
2025-10-17 15:32:14 +0530

    Add files via upload

---
+++ PROJECT3/project3/src/App.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [user, setUser] = useState({});
+
+  const searchGethub = async (username) => {
+    const { data } = await axios.get(
+      "https://github.com/Kumar-dolly"
+    );
+
+    console.log(data);
+
+    setUser(data);
+    console.log(user);
+  };
+  searchGithub();
+  return (
+    <>
+      <h1>Github Search</h1>
+      <img src={user.avatar_url} alt=""width={"10%"} />
+      <h2>{user.name}</h2>
+      <p>{user.bio}</p>
+    </>
+  );
+};
+export default App;
---
+++ PROJECT3/project3/src/Pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About Page</h1>
+      <Link to={"/"}>Goto Home Page</Link>
+    </>)
+}
+export default AboutPage;
