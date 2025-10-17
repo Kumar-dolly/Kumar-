01f4792e7154e1a25cdd05a71e583a08014c8da2
Kumar.S <kumar17112006@gmail.com>
2025-10-17 15:30:12 +0530

    Add files via upload

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <>
+      <Header />{" "}
+      <div>
+        <h1>Hello From React</h1>
+        <p>I am Kumar , From IOT </p>
+      </div>
+    </>
+  );
+}
+export default App;
